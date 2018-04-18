b922ad35d11715edbc025d570aeedd3148291751
claytonSMichael <clayton.michael@uconn.edu>
2018-04-18 16:08:36 -0400

    moved files to new directorY

---
+++ CSE/3666/core_sc.py
@@ -0,0 +1,193 @@
+from hardware import Memory, RegisterFile, Register, MUX_2_1, ALU_32, AND_2
+import utilities 
+from signals import Signals
+
+class Core_SC: 
+    def __init__(self):
+        self.I_Mem = Memory()
+        self.D_Mem = Memory()
+        self.RF = RegisterFile()
+        self.RegPC = Register()
+        self.signals = Signals()
+        self.cycle_num = 0
+        self.mode = 0
+
+    def set_PC(self, pc):
+        self.RegPC.set_data(pc)
+        self.RegPC.set_write(1)
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    def run(self, n_cycles):
+        i_cycles = 0
+        ending_PC = self.I_Mem.get_ending_address() 
+
+        self.I_Mem.set_memread(1)
+        self.I_Mem.set_memwrite(0)
+
+        while (n_cycles == 0 or i_cycles < n_cycles):
+            i_cycles += 1
+            self.cycle_num += 1
+            if ((self.mode & 2) == 0): utilities.print_new_cycle(self.cycle_num)
+
+            # clock changes
+            self.RegPC.clock()
+            self.RF.clock()
+
+            # read PC
+            self.signals.PC = self.RegPC.read()
+            self.signals.PC_4 = self.signals.PC_new = self.signals.PC + 4
+            if ((self.mode & 2) == 0): utilities.println_int("PC", self.signals.PC)
+            if (self.signals.PC > ending_PC): 
+                if ((self.mode & 2) == 0): print("No More Instructions")
+                i_cycles -= 1 
+                break
+
+            self.I_Mem.set_address(self.signals.PC)
+            self.I_Mem.run()
+            self.signals.instruction = self.I_Mem.get_data() 
+
+            if ((self.mode & 2) == 0): utilities.println_int("instruction", self.signals.instruction)
+
+            # Now you have PC and the instruction
+            # Some signals' value can be extracted from instruction directly 
+            self.signals_from_instruction(self.signals.instruction, self.signals)
+
+            # call main_control
+            self.main_control(self.signals.opcode, self.signals)
+            
+            # call sign_extend
+            self.signals.Sign_extended_immediate = self.sign_extend(self.signals.immediate)
+            
+            # Write_register. Also an example of using MUX
+            self.signals.Write_register = MUX_2_1(self.signals.rt, self.signals.rd, self.signals.RegDst) 
+            
+            # ALU control
+            self.signals.ALU_operation = self.ALU_control(self.signals.ALUOp, self.signals.funct)
+            
+            # Calculate branch address 
+            self.signals.Branch_address = self.calculate_branch_address(self.signals.PC_4, self.signals.Sign_extended_immediate)
+            self.signals.Jump_address = self.calculate_jump_address(self.signals.PC_4, self.signals.instruction)
+            
+            # Print out signals generated in Phase 1.
+            if ((self.mode & 4) == 0): utilities.print_signals_1(self.signals)
+
+            # If phase 1 only, continue to the next instruction.
+            if ((self.mode & 1) != 0):
+                self.RegPC.set_data(self.signals.PC_4)
+                self.RegPC.set_write(1)
+                continue
+            
+            # You will continue to complete the core in phase 2
+            # Use RF, ALU, D_Mem 
+            # Preapre RF write 
+            # Compute PC_new 
+
+            self.RegPC.set_data(self.signals.PC_new)
+            self.RegPC.set_write(1)
+
+            # Print out signals generated in Phase 2.
+            if ((self.mode & 8) == 0): utilities.print_signals_2(self.signals)
+        return i_cycles
+            
+    def signals_from_instruction (self, instruction, sig):
+		
+		sig.opcode = (instruction >> 26) & 0x3F
+		hex_4 = 0x457
+		hex_5 = 0x3F
+		hex_16 = 0xFFFF
+
+		if not sig.opcode:
+			sig.rs = ((instruction << 6) >> 27) & hex_4
+			sig.rt = ((instruction << 11) >> 27) & hex_4
+			sig.rd = ((instruction << 16) >> 27) & hex_4
+			sig.funct = ((instruction << 26) >> 26) & hex_5
+		elif not sig.opcode & 0x02:
+			sig.rs = ((instruction << 6) >> 27) & hex_4
+			sig.rt = ((instruction << 11) >> 27) & hex_4	
+			sig.immediate = ((instruction << 16) >> hex_16
+		else:
+			pass
+			
+    def main_control(self, opcode, sig):
+        """
+        Check the type of input instruction
+        """
+        #set defaults for control signals 
+        sig.RegDst = sig.Jump = sig.Branch = sig.MemRead = sig.MemtoReg = sig.ALUOp = sig.MemWrite = sig.ALUSrc = sig.RegWrite = 0
+
+        #determine control signals
+        if not opcode:             
+			sig.RegWrite = 1
+			sig.RegDst = 1
+			sig.ALUOp = 2
+	
+		elif not (opcode & 0x23):
+			sig.MemRead = 1
+			sig.MemtoReg = 1
+			sig.ALUSrc = 1
+			sig.RegWrite 1
+	
+		elif not (opcode & 0x2B):
+			sig.ALUSrc = 1
+			sig.MeMWrite = 1
+		elif not (opcode & 0x4):
+			sig.Jump = 1
+		else:
+			sig.RegWrite = 1
+			sig.ALUSrc = 1
+			sig.Branch = 1
+	
+    def ALU_control(self, alu_op, funct):  
+        """
+        Get alu_control from func field of instruction
+        Input: function field of instruction
+        Output: alu_control_out
+       
+        """
+        alu_control_out = 0
+        # One example is given, continue to finish other cases.
+        if alu_op == 0:             # 00  
+			alu_control_out = 2     # 0010
+		elif alu_op == 1:
+			alu_control_out = 6
+		else alu_op == 2:
+			if funct == 32:
+				alu_control_out = 2
+			elif funct == 34:
+				alu_control_out = 6
+			elif funct == 36:
+				alu_control_out = 0
+			elif funct == 37:
+				alu_control_out = 1
+			else:
+				alu_control_out = 7	        
+
+        return alu_control_out
+
+    def sign_extend(self, immd):
+        """
+        Sign extend module. 
+        Convert 16-bit to an int.
+        Extract the lower 16 bits. 
+        If bit 15 of immd is 1, compute the correct negative value (immd - 0x10000).
+        """
+        if immd < 0:
+			immd & 0xFFFF
+		else:
+			immd & 0x0000
+
+        return immd
+
+    def calculate_branch_address(self, pc_4, extended):
+		addr = 0
+		addr += (extended << 2) + pc_4
+        return addr
+
+    def calculate_jump_address(self, pc_4, instruction):
+        addr = 0
+		addr += instruction << 2
+		addr += ((pc_4 >> 28) << 28)  
+        return addr
+
